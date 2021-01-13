0940f58ee4d299791c6bf48ecd69b877bacef4db
Aidan <aidan.gill@oncallhealth.co.nz>
2021-01-13 14:06:44 +0000

    floating button + black background for giphy picker

--- lib/src/widgets/giphy_preview_page.dart
+++ lib/src/widgets/giphy_preview_page.dart
@@ -8,29 +8,33 @@ class GiphyPreviewPage extends StatelessWidget {
   final Widget title;
   final ValueChanged<GiphyGif> onSelected;
 
-  const GiphyPreviewPage(
-      {@required this.gif, @required this.onSelected, this.title});
+  const GiphyPreviewPage({@required this.gif, @required this.onSelected, this.title});
 
   @override
   Widget build(BuildContext context) {
     final media = MediaQuery.of(context);
 
     return Scaffold(
-        appBar: AppBar(title: title, toolbarHeight: 50, actions: <Widget>[
-          IconButton(icon: Icon(Icons.check), onPressed: () => onSelected(gif))
-        ]),
+        appBar: AppBar(
+            title: title,
+            toolbarHeight: 55),
         body: SafeArea(
-            child: Center(
-                child: GiphyImage.original(
-              gif: gif,
-              width: media.orientation == Orientation.portrait
-                  ? double.maxFinite
-                  : null,
-              height: media.orientation == Orientation.landscape
-                  ? double.maxFinite
-                  : null,
-              fit: BoxFit.contain,
-            )),
-            bottom: false));
+            child: Container(
+                color: Colors.black,
+                child: Center(
+                    child: GiphyImage.original(
+                  gif: gif,
+                  width: media.orientation == Orientation.portrait ? double.maxFinite : null,
+                  height: media.orientation == Orientation.landscape ? double.maxFinite : null,
+                  fit: BoxFit.contain,
+                ))),
+            bottom: false),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            onSelected(gif);
+          },
+          child: Icon(Icons.check),
+          backgroundColor: Color.fromARGB(255, 44, 149, 215),
+        ));
   }
 }
